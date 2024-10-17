fe7bb23bbf90ff7499325fe01f822f54cb33bcd3
Javier Jiménez <javierjimenezcarralero@gmail.com>
2024-10-17 13:29:33 +0200

    Sequalize-init

--- src/backend-node/models/index.js
+++ src/backend-node/models/index.js
@@ -1,7 +1,21 @@
-const { Sequalize } = require('sequalize');
+const { Sequelize } = require('sequelize');
 const UserModel = require('./user.model');
 const ClientExpedienteModel = require('./clientExpediente.model');
 const ClientModel = require('./client.model');
 const ExpedienteModel = require('./expediente.model');
 const DeudaModel = require('./deuda.model');
 const PagoModel = require('./pago.model');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: 'false',
+});
+
+const models = {
+    User: UserModel(sequelize),
+    ClientExpediente: ClientExpedienteModel(sequelize),
+    Client: ClientModel(sequelize),
+    Expediente: ExpedienteModel(sequelize),
+    Deuda: DeudaModel(sequelize),
+    Pago: PagoModel(sequelize),
+};
