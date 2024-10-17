a9407e603ab21140a1a2ff8edfaefc1b60fec69d
Javier Jiménez <javierjimenezcarralero@gmail.com>
2024-10-17 13:28:59 +0200

    Sequalize-init

---
+++ src/backend-node/models/index.js
@@ -0,0 +1,7 @@
+const { Sequalize } = require('sequalize');
+const UserModel = require('./user.model');
+const ClientExpedienteModel = require('./clientExpediente.model');
+const ClientModel = require('./client.model');
+const ExpedienteModel = require('./expediente.model');
+const DeudaModel = require('./deuda.model');
+const PagoModel = require('./pago.model');
